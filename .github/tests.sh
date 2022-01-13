400bf99111f44e8c5910db68e135041be659bf8f
Marvin Moldenhauer <39803750+Xenion1987@users.noreply.github.com>
2022-01-13 22:27:39 +0100

    extend test

--- .github/tests.sh
+++ .github/tests.sh
@@ -3,15 +3,27 @@
 # run docker-compose tests
 #
 
+set -x
 set -o errexit
 set -o pipefail
 
 docker-compose logs --timestamps --follow &
 
 if docker-compose up --detach; then
+    unset i
+    for ((i=1;i<=60;i++)); do 
+        echo -n "Health-Check ${i} :: "
+        CONTAINER_STATUS=$(docker ps --filter "name=$(grep ENV_ENV_CONTAINER_NAME .env | awk -F= '{print $2}')" --format "{{.Status}}"| grep -Eo "\((.*)\)")
+        echo "${CONTAINER_STATUS}"
+        if [[ ${CONTAINER_STATUS} == "(healthy)" ]]; then
+            break
+        fi
+        sleep 1
+    done
+    SEARCH_PATTERN="Authentication key needed to continue"
     CURL_CHECK=$(curl -si --url "http://localhost:$(grep ENV_LOCAL_WEB_PORT .env | awk -F= '{print $2}')/admin" | \
-        grep -o "Authentication key needed to continue")
-    if [[ "${CURL_CHECK}" == "Authentication key needed to continue" ]]; then
+        grep -o "${SEARCH_PATTERN}")
+    if [[ "${CURL_CHECK}" == "${SEARCH_PATTERN}" ]]; then
         exit 0
     fi
  else
