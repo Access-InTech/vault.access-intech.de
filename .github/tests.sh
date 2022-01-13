2eabb12b7bcbb93bd017a50fabad40fa55e85a44
Marvin Moldenhauer <39803750+Xenion1987@users.noreply.github.com>
2022-01-13 21:48:41 +0100

    add check for admin page

--- .github/tests.sh
+++ .github/tests.sh
@@ -9,7 +9,11 @@ set -o pipefail
 docker-compose logs --timestamps --follow &
 
 if docker-compose up --detach; then
-    exit 0
+    CURL_CHECK=$(curl -si --url "http://localhost:$(grep ENV_LOCAL_WEB_PORT .env | awk -F= '{print $2}')/admin" | \
+        grep -o "Authentication key needed to continue")
+    if [[ "${CURL_CHECK}" == "Authentication key needed to continue" ]]; then
+        exit 0
+    fi
  else
     exit 255
 fi
